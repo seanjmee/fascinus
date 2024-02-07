8abe9ca350262f377d2143a65a9bf7ee8d68f699
Sean Mee <sean.mee@hotmail.com>
2024-02-07 00:42:43 -0500

    Initial Commit

---
+++ fascinus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fascinus
+//
+//  Created by Sean Mee on 2024-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fascinus/fascinusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fascinusApp.swift
+//  fascinus
+//
+//  Created by Sean Mee on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct fascinusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
